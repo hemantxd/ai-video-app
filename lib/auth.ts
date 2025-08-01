1b339f516b264d8d0ae34dd7e0e633f81f1cb5b6
Hemant Bisht <bishthemant169@gmail.com>
2025-08-01 23:24:02 +0530

    imagekit installation

--- lib/auth.ts
+++ lib/auth.ts
@@ -69,8 +69,14 @@ export const authOptions: NextAuthOptions = {
   },
 
   pages:{
-
-  }
+    signIn: "/login",
+    error: "/login"
+  },
+  session:{  //optional
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60
+  },
+  secret: process.env.NEXTAUTH_SECRET
 
 };
 
